8cfbbc9e5fe879ceca5998435f3cec74b1d96f06
Ard-Jan van Etten <ardjanve@gmail.com>
2021-06-07 14:00:37 +0200

    e

--- download_treinen_vandaag.js
+++ download_treinen_vandaag.js
@@ -2,12 +2,13 @@ const haalDataOp = require('./functies/haalDataOp.js');
 const writeJSON = require('./functies/writeJSON.js');
 const readJSON = require('./functies/readJSON.js');
 
+process.on('uncaughtException', console.log(error.stack));
 
 (async () => {
     const ritten = await Promise.all((await readJSON('alleritjes'))
         .map((rit) => rit.ritnummer)
         .filter((ritnummer, index, array) => array.indexOf(ritnummer) == index)
-        .map(async (ritnummer) => await haalDataOp(`/virtual-train-api/api/v1/trein/${ritnummer}`)));
+        .map((ritnummer) => haalDataOp(`/virtual-train-api/api/v1/trein/${ritnummer}`)));
 
     await writeJSON(ritten, 'ritten');
 })();
